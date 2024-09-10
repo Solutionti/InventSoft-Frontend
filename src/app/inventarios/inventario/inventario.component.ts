d7b44c27661ca5d1bf22726cf068fed8eb9b140c
Cristian Vargas <ccvargas95@gmail.com>
2024-09-09 22:32:17 -0500

    subiendo

--- src/app/inventarios/inventario/inventario.component.ts
+++ src/app/inventarios/inventario/inventario.component.ts
@@ -1,5 +1,6 @@
+import { InventarioService } from './../services/inventario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MenuComponent } from "../../componentes/menu/menu.component";
 
 @Component({
@@ -14,7 +15,9 @@ import { MenuComponent } from "../../componentes/menu/menu.component";
 })
 export class InventarioComponent implements OnInit {
 
-  constructor(){
+  constructor(
+    private InventarioServices: InventarioService
+  ){
 
   }
 
@@ -23,18 +26,33 @@ export class InventarioComponent implements OnInit {
   }
 
   salidaForm = new FormGroup ({
-
-
+    producto_salida: new FormControl(''),
+    cantidad_salida: new FormControl(''),
+    stock_salida: new FormControl(''),
+    seccion_salida: new FormControl(''),
+    motivo_salida: new FormControl(''),
+    comentarios_salida: new FormControl(''),
   });
 
   entradaForm = new FormGroup ({
-
-
+    producto_entrada: new FormControl(''),
+    cantidad_entrada: new FormControl(''),
+    stock_entrada: new FormControl(''),
+    seccion_entrada: new FormControl(''),
+    motivo_entrada: new FormControl(''),
+    comentarios_entrada: new FormControl(''),
   });
 
   productoForm = new FormGroup ({
-
-
+    categoria_productos: new FormControl(''),
+    nombre_productos: new FormControl(''),
+    codigo_productos: new FormControl(''),
+    barras_productos: new FormControl(''),
+    medida_productos: new FormControl(''),
+    cantidad_productos: new FormControl(''),
+    precio_productos: new FormControl(''),
+    moneda_productos: new FormControl(''),
+    descripcion_productos: new FormControl(''),
   });
 
 
