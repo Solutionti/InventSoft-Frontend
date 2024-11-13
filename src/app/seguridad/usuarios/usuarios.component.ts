aa179a8f2a95e980ad90be0a0ce0ce04624106e6
Cristian Vargas <ccvargas95@gmail.com>
2024-11-13 00:13:19 -0500

    subiendo

--- src/app/seguridad/usuarios/usuarios.component.ts
+++ src/app/seguridad/usuarios/usuarios.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { SeguridadService } from './../services/seguridad.service';
+import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from "../../componentes/menu/menu.component";
+import { TableModule } from 'primeng/table';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [
+    MenuComponent,
+    TableModule,
+  ],
   templateUrl: './usuarios.component.html',
-  styleUrl: './usuarios.component.css'
+  styleUrl: './usuarios.component.css',
+  providers:[ConfirmationService,MessageService]
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit{
+
+  constructor(
+    private SeguridadService: SeguridadService,
+    private MessageService: MessageService,
+    private ConfirmationService: ConfirmationService
+  ){}
+
+  ngOnInit(): void {
+
+    this.getUsuarios()
+  }
+  getUsuario: any [] = [];
+  getUsuarios(){
+    this.SeguridadService
+        .getUsuarios()
+        .subscribe((response: any )=>{
+          this.getUsuario = response;
+        })
+  }
+
+
 
 }
