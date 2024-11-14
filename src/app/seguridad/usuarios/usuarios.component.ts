13d279a4b41e1753da623a8edd24cb7dcb74b1b8
Cristian Vargas <ccvargas95@gmail.com>
2024-11-14 00:22:33 -0500

    subiendo

--- src/app/seguridad/usuarios/usuarios.component.ts
+++ src/app/seguridad/usuarios/usuarios.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from "../../componentes/menu/menu.component";
 import { TableModule } from 'primeng/table';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+
 
 
 @Component({
@@ -11,6 +14,7 @@ import { ConfirmationService, MessageService } from 'primeng/api';
   imports: [
     MenuComponent,
     TableModule,
+    ReactiveFormsModule
   ],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css',
@@ -28,6 +32,19 @@ export class UsuariosComponent implements OnInit{
 
     this.getUsuarios()
   }
+
+  usuariosForm: FormGroup = new FormGroup({
+    usuario_nombre: new FormControl(''),
+    usuario_apellido: new FormControl(''),
+    usuario_user: new FormControl(''),
+    usuario_contraseña: new FormControl(''),
+    usuario_correo: new FormControl(''),
+    usuario_telefono: new FormControl(''),
+    usuario_empresa: new FormControl(''),
+    usuario_rol: new FormControl(''),
+    usuario_estado: new FormControl(''),
+  });
+
   getUsuario: any [] = [];
   getUsuarios(){
     this.SeguridadService
